b8cf5667840917a93a2c1b75240ba4718c8bdb37
Douglas Cook <douglas.cook13@imperial.ac.uk>
2017-03-09 19:39:27 +0000

    Cube roots!

---
+++ chapter-1/ex-1-8.scm
@@ -0,0 +1,29 @@
+; Define a procedure to calculate cube roots using Newton's method
+
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (cube x)
+  (* x x x))
+
+
+; with improved good enough check
+(define (cube-root x)
+
+  (define (cube-iter guess previous-guess)
+
+    (define (good-enough?)
+      (< (abs (- guess previous-guess))
+         (/ guess 10000)))
+
+    (define (improve-guess)
+      (/ (+ (/ x (square guess))
+            (* 2 guess))
+          3))
+
+    (if (good-enough?)
+      guess
+      (cube-iter (improve-guess) guess)))
+
+  (cube-iter 1.0 0))
